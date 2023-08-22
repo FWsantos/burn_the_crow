fcfa3b55bd7dce9e66fe68e04e76943e3cee3c22
Wesley <fwsantos23@gmail.com>
2023-08-22 15:41:47 -0300

    Crow adicionado ao projeto

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <crow.h>
+int main(int, char **)
+{
+    crow::SimpleApp app; // define your crow application
+
+    // define your endpoint at the root directory
+    CROW_ROUTE(app, "/")
+    ([]()
+     { return "Hello word"; });
+
+    // set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
+}
